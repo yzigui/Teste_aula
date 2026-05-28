c48150e69b01e1db5cb1a7d25d641cef15f6a4e2
yzigui <yasmimguimaraes883@gmail.com>
2026-05-28 14:04:43 +0100

    clone
    ...

--- main.py
+++ main.py
@@ -12,3 +12,4 @@ print("É estranho pensar que o computador só entende isso porque alguém inven
 print("É curioso como a gente passa a vida inteira tentando descobrir quem é, mas quase nunca sabe o que responder quando alguém pergunta.")
 print("Professor, se errar faz parte do aprendizado, então um código cheio de erros significa que estou aprendendo muito rápido?")
 print("Não sei mais o que escrever, então vou começar a numerar as próximas linhas.")
+10
